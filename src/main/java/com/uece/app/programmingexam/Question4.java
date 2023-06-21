ffabf7999b8bf6758e7e264a7c05282be597554d
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2023-06-21 01:46:29 +0000

    :fix: fix: solution init

--- src/main/java/com/uece/app/programmingexam/Question4.java
+++ src/main/java/com/uece/app/programmingexam/Question4.java
@@ -1,7 +1,11 @@
 package com.uece.app.programmingexam;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Question4{
     public static void main(String[] args) {
+        Random random = new Random();
+         int[] matriz = new int[n];
         System.out.println("Hello,world!");
 }
 }
@@ -10,3 +14,5 @@ public class Question4{
 preencha-a com valores gerados aleatoriamente no intervalo de 1 a 10 e apresente-a. Em
 seguida, troque os elementos da diagonal principal com os da secundária, em cada linha,
 e apresente novamente a matriz. */
+
+/*Criar um objeto random */
