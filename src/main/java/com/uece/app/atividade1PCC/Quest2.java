193d23aef43b882a27904019ed3fe1db5a7844ef
KilviaSantos <kilviamssantos@gmail.com>
2023-08-28 17:43:11 +0000

    :books: Question 2 finalizada - melhorar

--- src/main/java/com/uece/app/atividade1PCC/Quest2.java
+++ src/main/java/com/uece/app/atividade1PCC/Quest2.java
@@ -1,17 +1,32 @@
 package com.uece.app.atividade1PCC;
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Quest2{
 
+   
     public static void main(String[] args){
         //Cria objeto Pessoa que será usado para input através do Scanner.
-        Scanner pessoa = new Scanner; 
-        //Recebe o nome (texto).
+        Scanner pessoa = new Scanner(System.in);           
         
-        //Recebe o cpf (texto).
-        //Recebe o sexo (caractere) 
-        //Recebe o ano de nascimento (inteiro) e armazene-os em um registro. Em seguida,
-        //Imprime o nome, o sexo e a idade dessa pessoa.
+        //Recebe o nome (texto).
+        System.out.println("Qual o seu nome?");
+        String nome = pessoa.nextLine();
+
+        //Recebe o cpf (texto)
+        System.out.println("Qual o seu CPF?");
+        String cpf = pessoa.nextLine();
+
+        //Recebe o sexo (caractere)
+        System.out.println("Qual o seu sexo?");
+        String s = pessoa.nextLine();
+        char sexo = s.charAt(0);
+
+        //Recebe o ano de nascimento (inteiro) e armazene-os em um registro
+        System.out.println("Qual o seu ano de nascimento?");
+        int anoNascimento = pessoa.nextInt();
+
+        //Imprime o nome, o sexo e a idade dessa pessoa
+        System.out.println(nome + ", sexo " + sexo + ", nasceu no ano " + anoNascimento + ".");
     }
     
 }
