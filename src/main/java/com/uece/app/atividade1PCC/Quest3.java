2901022fb83fc319d53e3ab385ee28c425e4c82a
KilviaSantos <kilviamssantos@gmail.com>
2023-08-28 19:40:50 +0000

    :books: Algoritmo question 3

---
+++ src/main/java/com/uece/app/atividade1PCC/Quest3.java
@@ -0,0 +1,30 @@
+package com.uece.app.atividade1PCC;
+import java.util.Scanner;
+
+public class Quest3 {
+   
+    public static void main(String[] args){
+
+    //Faça um algoritmo chamado Calculadora 
+    //System.out.println("Bemvindo(a) a sua Calculadora. Que operação você deseja executar?");
+    //string[] operacoesStrings;
+    //operacoesStrings = new string[4];
+    //string[] operacoes = {soma, subtração, multiplicação, divisão};
+    //System.out.println(operacoesStrings);
+    //Exibe as opções de operações (soma, subtração, multiplicação e divisão) 
+
+    //Solicita dois números do usuário
+    Scanner numero = new Scanner(System.in);
+    int num1, num2;
+    //Primeiro número
+    System.out.println("Digite o primeiro número a ser utilizado na operação:");
+    num1 = numero.nextInt();
+    
+    //Segundo número
+    System.out.println("Digite o segundo número a ser utilizado na operação:");
+    num2 = numero.nextInt();
+    
+    
+    //Cada operação deverá ser criada em um procedimento (sub-rotina).
+    }
+}
