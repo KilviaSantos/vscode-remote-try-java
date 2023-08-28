9e6e23439961733c53dbf817868534fd5f25b3d6
Kilvia Santos <58449533+KilviaSantos@users.noreply.github.com>
2023-08-28 15:06:15 +0000

    :books: Create question 2

---
+++ src/main/java/com/uece/app/atividade1PCC/Quest2.java
@@ -0,0 +1,17 @@
+package com.uece.app.atividade1PCC;
+import java.util.scanner;
+
+public class Quest2{
+
+    public static void main(String[] args){
+        //Cria objeto Pessoa que será usado para input através do Scanner.
+        Scanner pessoa = new Scanner; 
+        //Recebe o nome (texto).
+        
+        //Recebe o cpf (texto).
+        //Recebe o sexo (caractere) 
+        //Recebe o ano de nascimento (inteiro) e armazene-os em um registro. Em seguida,
+        //Imprime o nome, o sexo e a idade dessa pessoa.
+    }
+    
+}
