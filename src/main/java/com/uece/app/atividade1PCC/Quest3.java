95558f40f1c1856657f7d398a9e04732a35f25d0
KilviaSantos <kilviamssantos@gmail.com>
2023-08-29 11:43:49 +0000

    :books: Encapsulated received numbers

--- src/main/java/com/uece/app/atividade1PCC/Quest3.java
+++ src/main/java/com/uece/app/atividade1PCC/Quest3.java
@@ -13,18 +13,34 @@ public class Quest3 {
     //System.out.println(operacoesStrings);
     //Exibe as opções de operações (soma, subtração, multiplicação e divisão) 
 
-    //Solicita dois números do usuário
-    Scanner numero = new Scanner(System.in);
-    int num1, num2;
-    //Primeiro número
-    System.out.println("Digite o primeiro número a ser utilizado na operação:");
-    num1 = numero.nextInt();
-    
-    //Segundo número
-    System.out.println("Digite o segundo número a ser utilizado na operação:");
-    num2 = numero.nextInt();
+    getNumbers();
     
     
     //Cada operação deverá ser criada em um procedimento (sub-rotina).
     }
+
+    //Solicita dois números do usuário
+    private static void getNumbers() {
+        Scanner numero = new Scanner(System.in);
+        int num1, num2;
+        //Primeiro número
+        System.out.println("Digite o primeiro número a ser utilizado na operação:");
+        num1 = numero.nextInt();
+        
+        //Segundo número
+        System.out.println("Digite o segundo número a ser utilizado na operação:");
+        num2 = numero.nextInt();
+      }
+
+    //Realiza soma com os números
+
+
+    //Realiza subtração com os números
+
+
+    //Realiza multiplicação com os números
+
+
+    //Realiza divisão com os números
+
 }
