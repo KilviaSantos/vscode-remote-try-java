80e68850c99bf54ca8d5131c0e3f218db9444b2c
KilviaSantos <kilviamssantos@gmail.com>
2023-08-31 16:27:10 -0300

    update quest3

--- src/main/java/com/uece/app/atividade1PCC/Quest3.java
+++ src/main/java/com/uece/app/atividade1PCC/Quest3.java
@@ -4,23 +4,42 @@ import java.util.Scanner;
 public class Quest3 {
    
     public static void main(String[] args){
+        // Solicita a operaçao do usuário
+        Scanner operacao = new Scanner(System.in);
+        System.out.println("Digite o número 1 para Adição, 2 para Subtração, 3 para Multiplicação ou 4 para Divisão.");
+        int opEscolha;
+        opEscolha = operacao.nextInt();
+            switch(opEscolha){
+            case 1:
+            getNumbers();
+            adicao();    
+                break;
 
-    //Faça um algoritmo chamado Calculadora 
-    //System.out.println("Bemvindo(a) a sua Calculadora. Que operação você deseja executar?");
-    //string[] operacoesStrings;
-    //operacoesStrings = new string[4];
-    //string[] operacoes = {soma, subtração, multiplicação, divisão};
-    //System.out.println(operacoesStrings);
-    //Exibe as opções de operações (soma, subtração, multiplicação e divisão) 
+            case 2:
+            getNumbers();
+            subtracao();     
+                break;
+                
+            case 3:
+            getNumbers();
+            multiplicacao();     
+                break;
+        
+            default:
+            getNumbers();
+            divisao(); 
+                break;
+                
+        }
 
-    getNumbers();
-    
-    
-    //Cada operação deverá ser criada em um procedimento (sub-rotina).
-    }
+        // String opString = "soma", "subtração", "multiplicação", "divisão"
+        //escolhaOp = operacao.nextLine();
 
-    //Solicita dois números do usuário
-    private static void getNumbers() {
+     } 
+     //Faça um algoritmo chamado Calculadora         
+     //Solicita dois números do usuário
+
+    public static int[] getNumbers(){
         Scanner numero = new Scanner(System.in);
         int num1, num2;
         //Primeiro número
@@ -30,17 +49,59 @@ public class Quest3 {
         //Segundo número
         System.out.println("Digite o segundo número a ser utilizado na operação:");
         num2 = numero.nextInt();
+        
+        return new int[]{num1, num2};
+
       }
 
     //Realiza soma com os números
 
+    public static void adicao(){
+        int[] numeros = getNumbers();
+
+        int num1= numeros[0];
+        int num2= numeros[1];
+        int resultado = num1 + num2;
+        System.out.println("A adição é: " + resultado);
+    }
 
     //Realiza subtração com os números
 
+    public static void subtracao(){
+        int[] numeros = getNumbers();
+        int num1= numeros[0];
+        int num2= numeros[1];
+        int resultado = num1 - num2;
+        System.out.println("A subtração é: " + resultado);
+
+    }
 
     //Realiza multiplicação com os números
 
+    public static void multiplicacao(){
+        int[] numeros = getNumbers();
+        int num1= numeros[0];
+        int num2= numeros[1];
+        int resultado = num1 * num2;
+        System.out.println("A multiplicação é: " + resultado);
+
+    }
 
     //Realiza divisão com os números
 
+    public static void divisao(){
+        int[] numeros = getNumbers();
+        int num1= numeros[0];
+        int num2= numeros[1];
+        int resultado = num1 / num2;
+        System.out.println("A divisão é: " + resultado);
+
+    }
+
+    //Vou fazer um switch para a escolha da operação a ser realizada
+
+    
+
+    
 }
+
