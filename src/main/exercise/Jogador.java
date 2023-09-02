e83ed7e7aedb5f3786024475e6b05029fc31665a
KilviaSantos <kilviamssantos@gmail.com>
2023-09-02 15:14:27 +0000

    :books: update

--- src/main/exercise/Jogador.java
+++ src/main/exercise/Jogador.java
@@ -1,6 +1,25 @@
 package com.uece.app.exercise;
 public class Jogador {
     
-    private TimeFutebol;
+    private TimeFutebol time;
+    private String nome;
+
+    public Jogador(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public TimeFutebol getTime(){
+        return this.time;
+    }
+
+    public void getTime(TimeFutebol time){
+        this.time = time;
+    }
+
+    
 
 }
--- src/main/exercise/Main.java
+++ src/main/exercise/Main.java
@@ -1,9 +1,31 @@
 package com.uece.app.exercise;
-
 import java.util;
+
 public class Main{
 public static void main(String[] args) {
     
+    TimeFutebol time = new TimeFutebol();
+
+    List<Jogador> lista = new ArrayList<>();
+
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+    lista.add(new Jogador("Artur"));
+
+    time.setJogadores(lista);
+
+    try{
+        System.out.println(null);
+    }
 } 
     
 }
