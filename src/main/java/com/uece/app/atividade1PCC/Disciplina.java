b89c2e11e2a8a37ef7a0a8b67cf780083d97fe9b
KilviaSantos <kilviamssantos@gmail.com>
2023-09-04 02:31:37 +0000

    :books: implementação classe Disciplina

--- src/main/java/com/uece/app/atividade1PCC/Disciplina.java
+++ src/main/java/com/uece/app/atividade1PCC/Disciplina.java
@@ -1,6 +1,15 @@
 package com.uece.app.atividade1PCC;
 
+import java.util.Scanner;
+
 //Criar uma Classe Disciplina
 public class Disciplina {
+
+    Scanner nome = new Scanner(System.in);
+    System.out.println("Digite seu nome");
+    String nome = nome.nextLine();
+    int quantidadeCreditos = quantidadeCreditos.nextInt;
+    int quantidadeHoras = quantidadeHoras;
+    System.out.println
 
 }
--- src/main/java/com/uece/app/atividade1PCC/Quest4.java
+++ src/main/java/com/uece/app/atividade1PCC/Quest4.java
@@ -1,5 +1,13 @@
 package com.uece.app.atividade1PCC;
+//import
 
 public class Quest4 {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
+//Faça um programa que armazene o nome (texto), a quantidade de créditos (inteiro) e a
+ //quantidade de horas (inteiro) de 5 disciplinas e armazene-as em um arquivo binário. Em
+ //seguida, apresente os dados de cada disciplina armazenados nesse arquivo.
